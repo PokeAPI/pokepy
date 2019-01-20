8199edb90c9de9a70c1c4acb69b0e51a3b470a33
Kronopt <Kronopt@users.noreply.github.com>
2019-01-20 21:35:53 +0000

    Update __init__.py

--- pokepy/__init__.py
+++ pokepy/__init__.py
@@ -16,7 +16,7 @@ Usage:
 __author__ = 'Paul Hallett'
 __email__ = 'hello@phalt.co'
 __credits__ = ["Paul Hallett", "Owen Hallett", "Kronopt"]
-__version__ = '0.4'
+__version__ = '0.5.0'
 __copyright__ = 'Copyright Paul Hallett 2016'
 __license__ = 'BSD'
 
